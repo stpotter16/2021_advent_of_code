161617e5deb8db6627668f0bd228fbc2ff7dfaee
Sam Potter <spotter1642@gmail.com>
2022-11-24 14:02:58 -0600

    AoC Day 1 Part 1 but with iterators

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -3,21 +3,17 @@ use std::fs;
 fn main() {
     let input_file = "./input/input.txt";
     let contents = fs::read_to_string(input_file).expect("Should be able to read file");
-    let depths = contents.lines().map(str::parse::<i64>).map(Result::unwrap).collect();
-    let count = find_increasing_depths(depths);
-
-    fn find_increasing_depths(depths: Vec<i64>) -> i64 {
-        let mut count: i64 = 0;
-        for i in 0..depths.len() - 1 {
-            let j = i + 1;
-            let cur = depths[i];
-            let next = depths[j];
-            if next > cur {
-                count += 1;
-            }
-        }
-        count
-    }
-    
-    println!("Final count: {}", count);
+    let (increases, _ )= contents.lines().map(str::parse::<i64>).map(Result::unwrap).fold(
+        (0, i64::MIN), |(increases, prev), curr| {
+            (
+                if prev < curr {
+                    increases + 1
+                } else {
+                    increases
+                },
+                curr,
+            )
+        });
+                
+    println!("Iter count: {}", increases - 1);
 }
